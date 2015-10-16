ba4b157453efea21482762a87fcf4e4329454440
Ben Blackburn <benjamin.blackburn@gmail.com>
2015-10-16 05:52:18 -0500

    adds validations for name and note

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,6 @@ class Entry < ActiveRecord::Base
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
 														 default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+	validates_presence_of :name, :note
 end
